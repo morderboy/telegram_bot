e0a5172000e0b798d5645e53f57a432cb90c0762
morderboy <slava137267@gmail.com>
2024-03-03 12:18:17 +1000

    add buy_tokens_kb to img gen

--- handlers.py
+++ handlers.py
@@ -87,7 +87,7 @@ async def generate_image(msg: Message):
     price = config.get_img_gen_price()
 
     if balance < price:
-        return await msg.answer(message.get_message("no money").format(price, balance))
+        return await msg.answer(message.get_message("no money").format(price, balance), reply_markup=kb.buy_tokens_kb)
 
     mesg = await msg.answer(message.get_message("gen wait"))
     res = await utils.generate_image(prompt=prompt)
